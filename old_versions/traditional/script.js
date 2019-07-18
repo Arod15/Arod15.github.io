15a4d19572ed49796e6be60fa3083b72d8844995
Andres Rodriguez <arod1198@gmail.com>
2019-07-18 17:31:42 -0400

    found a better color?

--- old_versions/traditional/script.js
+++ old_versions/traditional/script.js
@@ -5,6 +5,10 @@ function fade(obj) {
 function fadein() {
 	let obj = document.getElementById('test');
 	console.log(obj);
-	setTimeout(function() {obj.style.visibility = "visible";}, 1000);
+	// setTimeout(function() {obj.style.visibility = "visible";}, 1000);
+	for (int i = 1; i < 10; i++) {
+		let opac = obj.style.opacity
+		setTimeout(function() {obj.style.opacity = ""})
+	}
 	console.log(obj.style.visibility);
 }
